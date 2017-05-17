db89b6a2e5a83154fd8cb034fafe1d46f58ff444
Dmitry Shechtman <damnian@gmail.com>
2017-05-18 02:31:49 +0300

    Add ProductDetector

---
+++ src/Net.Chdk.Detectors.Software.Binary/ProductDetector.cs
@@ -0,0 +1,61 @@
+﻿using Net.Chdk.Model.Card;
+using Net.Chdk.Model.Software;
+using Net.Chdk.Providers.Boot;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+
+namespace Net.Chdk.Detectors.Software.Product
+{
+    public abstract class ProductDetector : IProductDetector
+    {
+        private IBootProvider BootProvider { get; }
+
+        protected ProductDetector(IBootProvider bootProvider)
+        {
+            BootProvider = bootProvider;
+        }
+
+        public SoftwareProductInfo GetProduct(CardInfo cardInfo)
+        {
+            var rootPath = cardInfo.GetRootPath();
+            var productPath = Path.Combine(rootPath, ProductName);
+            if (!Directory.Exists(productPath))
+                return null;
+
+            return new SoftwareProductInfo
+            {
+                Name = ProductName,
+                Version = GetVersion(rootPath),
+                Created = GetCreationTime(cardInfo),
+                Language = GetLanguage(rootPath),
+            };
+        }
+
+        protected abstract string ProductName { get; }
+
+        protected abstract Version GetVersion(string rootPath);
+
+        protected abstract CultureInfo GetLanguage(string rootPath);
+
+        protected static T GetValue<T>(string basePath, IDictionary<string, string> mapping, Func<string, T> getValue)
+            where T : class
+        {
+            foreach (var kvp in mapping)
+            {
+                var filePath = Path.Combine(basePath, kvp.Key);
+                if (File.Exists(filePath))
+                    return getValue(kvp.Value);
+            }
+            return null;
+        }
+
+        private DateTime GetCreationTime(CardInfo cardInfo)
+        {
+            var rootPath = cardInfo.GetRootPath();
+            var diskbootPath = Path.Combine(rootPath, BootProvider.FileName);
+            return File.GetCreationTimeUtc(diskbootPath);
+        }
+    }
+}
